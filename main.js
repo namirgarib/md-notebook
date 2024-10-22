95efcf38807f7be658a3d745fa450f887bfc83ba
Ejub Sabic <ejub1946@outlook.com>
2024-10-22 20:43:02 +0200

    file operations work now

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, Menu, dialog } = require("electron");
+const fs = require('fs');
 const path = require("path");
 
 let mainWindow;
@@ -15,8 +16,161 @@ function createWindow() {
     });
 
     mainWindow.loadFile('index.html');
+    
+    const isMac = process.platform === 'darwin'
 
-    //mainWindow.webContents.openDevTools();
+    const template = [
+    ...(isMac
+        ? [{
+            label: app.name,
+            submenu: [
+            { role: 'about' },
+            { type: 'separator' },
+            { role: 'services' },
+            { type: 'separator' },
+            { role: 'hide' },
+            { role: 'hideOthers' },
+            { role: 'unhide' },
+            { type: 'separator' },
+            { role: 'quit' }
+            ]
+        }]
+        : []),
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'Open File',
+                click: () => {
+                dialog.showOpenDialog({
+                    properties: ['openFile'],
+                    filters: [
+                        { name: 'Markdown file', extensions: ['md'] },
+                    ]
+                }).then(result => {
+                    if (!result.canceled) {
+                    const filePath = result.filePaths[0];
+                    fs.readFile(filePath, 'utf-8', (err, data) => {
+                        if (err) {
+                        console.error('Error reading file:', err);
+                        return;
+                        }
+                        mainWindow.webContents.send('file-opened', data);
+                    });
+                    }
+                }).catch(err => {
+                    console.error('Error selecting file:', err);
+                });
+                }
+            },
+            {
+                label: 'Save File',
+                click: () => {
+                dialog.showSaveDialog({
+                    title: 'Save File',
+                    filters: [
+                        { name: 'Markdown file', extensions: ['md'] },
+                    ]
+                }).then(result => {
+                    if (!result.canceled) {
+                    const filePath = result.filePath;
+                    if (filePath) {
+                        mainWindow.webContents.send('request-file-save', filePath);
+                    }
+                    }
+                }).catch(err => {
+                    console.error('Error saving file:', err);
+                });
+                }
+            },
+            { type: 'separator' },
+            {
+                label: 'Exit',
+                role: 'quit'
+            },
+            isMac ? { role: 'close' } : { role: 'quit' }
+        ]
+    },
+    // { role: 'editMenu' }
+    {
+        label: 'Edit',
+        submenu: [
+        { role: 'undo' },
+        { role: 'redo' },
+        { type: 'separator' },
+        { role: 'cut' },
+        { role: 'copy' },
+        { role: 'paste' },
+        ...(isMac
+            ? [
+                { role: 'pasteAndMatchStyle' },
+                { role: 'delete' },
+                { role: 'selectAll' },
+                { type: 'separator' },
+                {
+                label: 'Speech',
+                submenu: [
+                    { role: 'startSpeaking' },
+                    { role: 'stopSpeaking' }
+                ]
+                }
+            ]
+            : [
+                { role: 'delete' },
+                { type: 'separator' },
+                { role: 'selectAll' }
+            ])
+        ]
+    },
+    // { role: 'viewMenu' }
+    {
+        label: 'View',
+        submenu: [
+        { role: 'reload' },
+        { role: 'forceReload' },
+        { role: 'toggleDevTools' },
+        { type: 'separator' },
+        { role: 'resetZoom' },
+        { role: 'zoomIn' },
+        { role: 'zoomOut' },
+        { type: 'separator' },
+        { role: 'togglefullscreen' }
+        ]
+    },
+    // { role: 'windowMenu' }
+    {
+        label: 'Window',
+        submenu: [
+        { role: 'minimize' },
+        { role: 'zoom' },
+        ...(isMac
+            ? [
+                { type: 'separator' },
+                { role: 'front' },
+                { type: 'separator' },
+                { role: 'window' }
+            ]
+            : [
+                { role: 'close' }
+            ])
+        ]
+    },
+    {
+        role: 'help',
+        submenu: [
+        {
+            label: 'Learn More',
+            click: async () => {
+            const { shell } = require('electron')
+            await shell.openExternal('https://electronjs.org')
+            }
+        }
+        ]
+    }
+    ]
+
+    const menu = Menu.buildFromTemplate(template)
+    Menu.setApplicationMenu(menu)
 }
 
 app.whenReady().then(createWindow);
--- renderer.js
+++ renderer.js
@@ -1,3 +1,5 @@
+const { ipcRenderer } = require('electron');
+const fs = require('fs');
 const textarea = document.getElementById('markdown');
 const preview = document.getElementById('preview');
 const marked = require('marked');
@@ -6,3 +8,18 @@ textarea.addEventListener('input', (e) => {
     const markdownText = e.target.value;
     preview.innerHTML = marked.parse(markdownText);
 });
+
+ipcRenderer.on('request-file-save', (event, filePath) => {
+    textarea.innerText = filePath;
+    fs.writeFile(filePath, textarea.innerText, (err) => {
+        if (err) {
+          return;
+        }
+    });
+});
+
+ipcRenderer.on('file-opened', (event, data) => {
+    textarea.innerHTML = data;
+    textarea.dispatchEvent(new Event('input'));
+});
+  
